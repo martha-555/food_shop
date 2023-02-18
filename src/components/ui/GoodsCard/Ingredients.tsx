5cc8551eb418cd2ce9c49b25689322c12fabc347
martha-555 <112080638+martha-555@users.noreply.github.com>
2023-02-19 00:41:49 +0200

    test

--- src/components/ui/GoodsCard/Ingredients.tsx
+++ src/components/ui/GoodsCard/Ingredients.tsx
@@ -2,7 +2,10 @@
 import Text from "../Text";
 import { useAppDispatch } from "../../../hooks/hooks";
 import { useSelector } from "react-redux";
-import { filterByIngredients, selectGoodsFilterIngredients } from "../../../store/GoodsStore";
+import {
+  filterByIngredients,
+  selectGoodsFilterIngredients,
+} from "../../../store/GoodsStore";
 import { Ingredients } from "../../../store/GoodsStore";
 import React from "react";
 
@@ -12,9 +15,11 @@ const FilterIngredients = () => {
 
   const clickHandler = (e: React.MouseEvent) => {
     const target = e.target as HTMLInputElement;
-    const clickedIngredient = target.value as Ingredients;;
+    const clickedIngredient = target.value as Ingredients;
 
-    const newIngredients = ingredients.filter(item => item !== clickedIngredient || target.checked);
+    const newIngredients = ingredients.filter(
+      (item) => item !== clickedIngredient || target.checked
+    );
 
     if (target.checked && !newIngredients.includes(clickedIngredient)) {
       newIngredients.push(clickedIngredient);
@@ -23,13 +28,32 @@ const FilterIngredients = () => {
     dispatch(filterByIngredients({ ingredients: newIngredients }));
   };
 
+  window.onload = function () {
+    const input = document.getElementsByTagName("input");
+    Array.from(input).forEach((item) => {
+      item.onchange = () =>
+        sessionStorage.setItem(item.id, item.checked.toString());
+      item.checked = sessionStorage.getItem(item.id) === "true";
+    });
+  };
+
   return (
     <div onClick={clickHandler}>
-      <input type="checkbox" value={Ingredients.mushrooms} id="" />
+      <input
+        type="checkbox"
+        value={Ingredients.mushrooms}
+        className="check"
+        id="1"
+      />
       <label htmlFor="">
         <Text>гриби</Text>{" "}
       </label>
-      <input type="checkbox" value={Ingredients.olives} id="" />
+      <input
+        type="checkbox"
+        value={Ingredients.olives}
+        className="check"
+        id="2"
+      />
       <label htmlFor="">
         <Text>маслини</Text>{" "}
       </label>
